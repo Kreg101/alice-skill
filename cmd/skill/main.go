8dceaa89283f62c606525cfeecafa792ef683756
Kreg101 <egorkravchenko321@yandex.ru>
2023-08-04 20:14:44 +0300

    add json body and compression

--- cmd/skill/main.go
+++ cmd/skill/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/Kreg101/alice-skill/internal/logger"
+	"github.com/Kreg101/alice-skill/internal/models"
 	"go.uber.org/zap"
 	"net/http"
+	"strings"
 )
 
 func main() {
@@ -22,8 +25,8 @@ func run() error {
 
 	logger.Log.Info("Running server", zap.String("address", flagRunAddr))
 
-	// оборачиваем хендлер webhook в middleware с логированием
-	return http.ListenAndServe(flagRunAddr, logger.RequestLogger(webhook))
+	// оборачиваем хендлер webhook в middleware с логированием и сжатием
+	return http.ListenAndServe(flagRunAddr, logger.RequestLogger(gzipMiddleware(webhook)))
 }
 
 func webhook(w http.ResponseWriter, r *http.Request) {
@@ -33,14 +36,68 @@ func webhook(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// десериализуем запрос в структуру модели
+	logger.Log.Debug("decoding request")
+	var req models.Request
+	dec := json.NewDecoder(r.Body)
+	if err := dec.Decode(&req); err != nil {
+		logger.Log.Debug("cannot decode request JSON body", zap.Error(err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// проверяем, что пришёл запрос понятного типа
+	if req.Request.Type != models.TypeSimpleUtterance {
+		logger.Log.Debug("unsupported request type", zap.String("type", req.Request.Type))
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		return
+	}
+
+	// заполняем модель ответа
+	resp := models.Response{
+		Response: models.ResponsePayload{
+			Text: "Извините, я пока ничего не умею",
+		},
+		Version: "1.0",
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	_, _ = w.Write([]byte(`
-          {
-            "response": {
-              "text": "Извините, я пока ничего не умею"
-            },
-            "version": "1.0"
-          }
-        `))
+
+	// сериализуем ответ сервера
+	enc := json.NewEncoder(w)
+	if err := enc.Encode(resp); err != nil {
+		logger.Log.Debug("error encoding response", zap.Error(err))
+		return
+	}
 	logger.Log.Debug("sending HTTP 200 response")
+}
+
+func gzipMiddleware(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ow := w
+
+		// проверяем, что клиент умеет получать от сервера сжатые данные в формате gzip
+		acceptEncoding := r.Header.Get("Accept-Encoding")
+		supportsGzip := strings.Contains(acceptEncoding, "gzip")
+		if supportsGzip {
+			cw := newCompressWriter(w)
+			ow = cw
+			defer cw.Close()
+		}
+
+		// проверяем, что клиент отправил серверу сжатые данные в формате gzip
+		contentEncoding := r.Header.Get("Content-Encoding")
+		sendsGzip := strings.Contains(contentEncoding, "gzip")
+		if sendsGzip {
+			cr, err := newCompressReader(r.Body)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			r.Body = cr
+			defer cr.Close()
+		}
+
+		h.ServeHTTP(ow, r)
+	}
 }
